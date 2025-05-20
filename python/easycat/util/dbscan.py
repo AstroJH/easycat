f9cfb2d5da26cab54772109245be4e66e1e9bcbb
Virjid <astrojh@163.com>
2025-05-20 22:44:41 +0800

    refine dbscan

--- python/easycat/util/dbscan.py
+++ python/easycat/util/dbscan.py
@@ -3,6 +3,7 @@ from astropy.coordinates import SkyCoord
 from astropy.units import Quantity
 import numpy as np
 import pandas as pd
+import numba
 
 
 def filter_dbscan(lcurve:pd.DataFrame, pos_ref:SkyCoord, radius:Quantity, min_neighbors:int=4, min_cluster_size=15):
@@ -16,16 +17,57 @@ def filter_dbscan(lcurve:pd.DataFrame, pos_ref:SkyCoord, radius:Quantity, min_ne
 
 
 def calc_distance(positions:SkyCoord):
-    N = len(positions)
+    # N = len(positions)
+    # distance = np.empty(shape=(N, N), dtype=np.float64)
+
+    # for i in range(0, N):
+    #     for j in range(i, N):
+    #         pos_i:SkyCoord = positions[i]
+    #         pos_j:SkyCoord = positions[j]
+
+    #         if i != j:
+    #             sep = pos_i.separation(pos_j).to_value("arcsec")
+    #         else:
+    #             sep = 0
+
+    #         distance[i, j] = sep
+    #         distance[j, i] = sep
+
+    pos_ra_deg  = positions.ra.to("deg").value
+    pos_dec_deg = positions.dec.to("deg").value
+    distance = calc_distance_njit(pos_ra_deg, pos_dec_deg)
+    
+    return distance
+
+
+@numba.njit
+def calc_separation_njit(ra1, dec1, ra2, dec2):
+    rad_ra1  = np.deg2rad(ra1)
+    rad_ra2  = np.deg2rad(ra2)
+    rad_dec1 = np.deg2rad(dec1)
+    rad_dec2 = np.deg2rad(dec2)
+
+    rad_theta = np.sqrt(
+        (rad_ra1-rad_ra2)**2 * np.cos(rad_dec1)**2
+            +
+        (rad_dec1-rad_dec2)**2
+    )
+
+    return np.rad2deg(rad_theta)*3600
+
+
+@numba.njit
+def calc_distance_njit(pos_ra_deg, pos_dec_deg):
+    N = len(pos_ra_deg)
     distance = np.empty(shape=(N, N), dtype=np.float64)
 
     for i in range(0, N):
         for j in range(i, N):
-            pos_i:SkyCoord = positions[i]
-            pos_j:SkyCoord = positions[j]
-
             if i != j:
-                sep = pos_i.separation(pos_j).to_value("arcsec")
+                sep = calc_separation_njit(
+                    pos_ra_deg[i], pos_dec_deg[i],
+                    pos_ra_deg[j], pos_dec_deg[j]
+                )
             else:
                 sep = 0
 
