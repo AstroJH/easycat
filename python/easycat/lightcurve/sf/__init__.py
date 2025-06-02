4c7065152faffe0102033517b8ea2680c4651792
Virjid <astrojh@163.com>
2025-06-02 21:20:06 +0800

    implement structure function

---
+++ python/easycat/lightcurve/sf/__init__.py
@@ -0,0 +1,4 @@
+from .core import sfdata, esfdata
+# from . import rust_sf
+
+__all__ = ["sfdata", "esfdata"]
--- python/easycat/lightcurve/sf/core.py
+++ python/easycat/lightcurve/sf/core.py
@@ -1,60 +1,77 @@
 import numpy as np
-from numpy import float64
-from numpy.typing import NDArray
+from typing import List
 
-import pandas as pd
-from pandas import DataFrame
+def sfdata(t, val, err, z:float=0.):
+    len_ts = len(t)
 
-
-def sfdata(ts:DataFrame, z:float):
-    len_ts = len(ts)
     if len_ts < 2:
-        return [],[],[]
-    
+        tmp = lambda _: [np.float64(x) for x in range(0)]
+        return tmp(), tmp(), tmp()
+
     len_tau = int(len_ts*(len_ts-1)/2)
 
-    t = ts["time"]
-    val = ts["val"]
-    err = ts["err"]
-
-    tau   = np.empty(len_tau, dtype=np.float64)
+    tau = np.empty(len_tau, dtype=np.float64)
     delta = np.empty(len_tau, dtype=np.float64)
     sigma = np.empty(len_tau, dtype=np.float64)
 
-    for i in range(0, len_ts-1):
-        span = len_ts - i
-        begin_pos = int((2*(len_ts-1)+2-i)*(i-1)/2)
-        end_pos = begin_pos + span
+    for i in range(len_ts-1):
+        span = len_ts - 1 - i
+        begin = i * len_ts - int(i*(i+1)/2)
+        end = begin + span
 
-        tau[begin_pos:end_pos]   = t[i+1:] - t[i]
-        delta[begin_pos:end_pos] = val[i+1:] - val[i]
-        sigma[begin_pos:end_pos] = np.sqrt(err[i]**2 + err[i+1:]**2)
-    
-    tau/(1+z), delta, sigma
+        tau[begin:end]   = t[i+1:] - t[i]
+        delta[begin:end] = val[i+1:] - val[i]
+        sigma[begin:end] = np.sqrt(err[i]**2 + err[i+1:]**2)
 
-def esfdata(ts_list:list[DataFrame], redshifts):
-    n_ts = len(ts_list)
-    len_ts = np.array([len(ts) for ts in ts_list])
-    len_tau = len_ts * (len_ts - 1) / 2
-    len_result = int(np.sum(len_tau))
+    return tau/(1+z), delta, sigma
 
-    tau   = np.empty(len_result, dtype=np.float64)
-    delta = np.empty(len_result, dtype=np.float64)
-    sigma = np.empty(len_result, dtype=np.float64)
 
-    for i in range(0, n_ts):
-        ts = ts_list[i]
-        z = redshifts[i]
-        begin_pos = 0
-        if i > 1:
-            begin_pos = np.sum(len_tau[1:i-1]) + 1
-        
-        end_pos = begin_pos + len_tau[i] - 1
+def esfdata(t_list, val_list, err_list, redshifts:List|float=0.):
+    if isinstance(redshifts, float):
+        redshifts = np.full(len(t_list), redshifts)
 
-        _tau, _delta, _sigma = sfdata(ts, z)
+    size_ts = np.array([len(t) for t in t_list])
+    size_tau = size_ts*(size_ts-1) >> 1
+    len_result = np.sum(size_tau)
 
-        tau[begin_pos:end_pos] = _tau
-        delta[begin_pos:end_pos] = _delta
-        sigma[begin_pos:end_pos] = _sigma
+    tau = np.empty(len_result)
+    delta = np.empty(len_result)
+    sigma = np.empty(len_result)
+
+    # traverse each lightcurve
+    begin = 0
+    for ele_size_tau, t, val, err, z in \
+        zip(size_tau, t_list, val_list, err_list, redshifts):
+        end = begin + ele_size_tau
+
+        _tau, _delta, _sigma = sfdata(t, val, err, z)
+        tau[begin:end] = _tau
+        delta[begin:end] = _delta
+        sigma[begin:end] = _sigma
+
+        begin = end
     
     return tau, delta, sigma
+
+
+def calc_sf(
+    tau, delta, sigma, bin_lo, bin_hi, sfmtd
+):
+    len_sf = len(bin_lo)
+    res_tau = np.empty(len_sf)
+    res_sf = np.empty(len_sf)
+    res_sigma = np.empty(len_sf)
+
+    for (i, lo, hi) in zip(range(len_sf), bin_lo, bin_hi):
+        mask = (tau >= lo) & (tau <= hi)
+
+        _tau = tau[mask]
+        _delta = delta[mask]
+        _sigma = sigma[mask]
+
+        result = sfmtd(_tau, _delta, _sigma)
+
+        res_tau[i], res_sf[i], res_sigma[i] = result
+    
+    mask = res_tau >= 0
+    return res_tau[mask], res_sf[mask], res_sigma[mask]
